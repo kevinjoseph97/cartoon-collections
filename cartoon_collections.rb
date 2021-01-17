8c05d3975ae47afc08d241d3c99b7e6f35ca154f
Kevin <kevinjoseph97@gmail.com>
2021-01-17 01:15:04 -0500

    commit tree 244cf6bda0819192e81ce3b1e9749326f4338d83

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,16 +1,35 @@
-def roll_call_dwarves# code an argument here
-  # Your code here
+def roll_call_dwarves(dwarves)
+  dwarves.each_with_index do |dwarves, index| 
+    print "#{index + 1}. #{dwarves} "
+  end
+
 end
 
-def summon_captain_planet# code an argument here
-  # Your code here
+
+
+
+
+def summon_captain_planet(veggies)
+
+  veggies.map {|veggies| veggies.capitalize! + "!"}
 end
 
-def long_planeteer_calls# code an argument here
-  # Your code here
+
+
+
+
+def long_planeteer_calls(calls_long)
+  calls_long.any? {|calls| calls.size > 4}
 end
 
-def find_the_cheese# code an argument here
-  # the array below is here to help
+
+
+
+
+
+def find_the_cheese(cheese)
   cheese_types = ["cheddar", "gouda", "camembert"]
-end
+  cheese.find do |cheese|
+    cheese_types.include?(cheese)
+  end
+end 
